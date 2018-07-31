14641710f28e4cdd387bc806e5dd82d47c481633
Amoury <ansar.memon@hotmail.com>
2018-07-31 18:01:09 +0400

    Initial Commit :tada:

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import App from './app/App';
+import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers/rootReducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware()
+));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
+
+registerServiceWorker();
